115d9d5375e2daa1d2ae240d55c3abe588e14ab9
15NgocAnh <Anhlee0125@gmail.com>
2024-04-19 10:43:33 +0700

    Update status 400 and reformat code

--- ConJob.API/Controllers/UserController.cs
+++ ConJob.API/Controllers/UserController.cs
@@ -1,15 +1,9 @@
-﻿
-using ConJob.Domain.DTOs.User;
+﻿using ConJob.Domain.DTOs.User;
 using ConJob.Domain.Services;
 using Microsoft.AspNetCore.Mvc;
 using static ConJob.Domain.Response.EServiceResponseTypes;
 using System.Security.Claims;
-using Microsoft.AspNetCore.Authorization;
-using Amazon.S3.Transfer;
-using Amazon.S3;
-using Amazon;
 using ConJob.Domain.Services.Interfaces;
-using ConJob.Domain.DTOs.Authentication;
 using ConJob.Domain.Response;
 
 namespace ConJob.API.Controllers
@@ -19,7 +13,7 @@ namespace ConJob.API.Controllers
     [Route("api/v{version:apiVersion}/[controller]/")]
     public class UserController : ControllerBase
     {
-        private readonly Microsoft.Extensions.Logging.ILogger _logger;
+        private readonly ILogger _logger;
         private readonly IUserServices _userServices;
         private readonly IS3Services _w3Services;
         public UserController(ILogger<UserController> logger, IUserServices userService, IS3Services w3Services)
@@ -32,8 +26,7 @@ namespace ConJob.API.Controllers
         [Route("update-profile")]
         [Produces("application/json")]
         [HttpPost]
-
-        public async Task<ActionResult> Update(UserInfoDTO userdata)
+        public async Task<ActionResult> updateUserProfile(UserInfoDTO userdata)
         {
             var claims = User.Claims.Where(x => x.Type == "UserId").FirstOrDefault();
             string? userid = claims == null ? null : claims.Value.ToString();
@@ -70,27 +63,30 @@ namespace ConJob.API.Controllers
         public async Task<ActionResult> getProfileUser()
         {
             var userid = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var serviceResponse = await _userServices.GetUserInfoAsync(userid);
-            return serviceResponse.ResponseType switch
+            if (userid == null)
             {
-                EResponseType.Success => Ok(serviceResponse.Data),
-                EResponseType.NotFound => BadRequest(serviceResponse.Message),
-                EResponseType.Forbid => Forbid(serviceResponse.Message),
-                _ => throw new NotImplementedException()
-            };
+                return BadRequest(new ServiceResponse<UserInfoDTO> { Message = "User Not Found!", ResponseType = EResponseType.BadRequest});
+            }
+            else
+            {
+                var serviceResponse = await _userServices.GetUserInfoAsync(userid);
+                return serviceResponse.ResponseType switch
+                {
+                    EResponseType.Success => Ok(serviceResponse.Data),
+                    EResponseType.NotFound => BadRequest(serviceResponse.Message),
+                    EResponseType.Forbid => Forbid(serviceResponse.Message),
+                    _ => throw new NotImplementedException()
+                };
+            }
         }
 
         [Route("avatar-upload")]
         [Produces("application/json")]
         [HttpPost]
-
         public async Task uploadAvatar(IFormFile file)
         {
             var userid = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-
             await _w3Services.UploadImage(file);
-
-
         }
 
 
