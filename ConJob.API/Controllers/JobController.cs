5bb7f86d5159c4d6a6e09e785f609a54e4e733ed
BanhmiqC140820 <143630995+mcongquang@users.noreply.github.com>
2024-04-19 10:58:33 +0700

    add get job

--- ConJob.API/Controllers/JobController.cs
+++ ConJob.API/Controllers/JobController.cs
@@ -41,6 +41,87 @@ namespace ConJob.API.Controllers
                 default:
                     throw new NotImplementedException();
             }
+            };
+        }
+        [HttpGet("get/{id}")]
+        [ProducesResponseType(typeof(ServiceResponse<JobDetailsDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServiceResponse<JobDetailsDTO>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ServiceResponse<JobDetailsDTO>), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> get(int id)
+        {
+            var serviceResponse = new ServiceResponse<JobDetailsDTO>();
+            serviceResponse = await _jobServices.GetJobAsync(id);
+            return serviceResponse.ResponseType switch
+            {
+                EResponseType.Success => Ok(serviceResponse),
+                EResponseType.BadRequest => BadRequest(serviceResponse),
+                EResponseType.CannotCreate => BadRequest(serviceResponse),
+                EResponseType.NotFound => NotFound(serviceResponse),
+                _ => throw new NotImplementedException()
+            };
+        }
+        [HttpGet("getAll")]
+        public async Task<IActionResult> getAll()
+        {
+            var serviceResponse = new ServiceResponse<IEnumerable<JobDTO>> ();
+            serviceResponse = await _jobServices.GetJobsAsync();
+            return serviceResponse.ResponseType switch
+            {
+                EResponseType.Success => Ok(serviceResponse),
+                EResponseType.BadRequest => BadRequest(serviceResponse),
+                EResponseType.CannotCreate => BadRequest(serviceResponse),
+                EResponseType.NotFound => NotFound(serviceResponse),
+                _ => throw new NotImplementedException()
+            };
+        }
+        [HttpPost("create")]
+        [ProducesResponseType(typeof(ServiceResponse<JobDetailsDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ServiceResponse<JobDetailsDTO>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ServiceResponse<JobDetailsDTO>), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Create([FromBody] JobDetailsDTO jobDTO)
+        {
+            var userid = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var serviceResponse = new ServiceResponse<JobDetailsDTO>();
+            serviceResponse = await _jobServices.AddJobAsync(1, jobDTO);
+            return serviceResponse.ResponseType switch
+            {
+                EResponseType.Success => Ok(serviceResponse.Data),
+                EResponseType.BadRequest => BadRequest(serviceResponse.Message),
+                EResponseType.CannotCreate => BadRequest(serviceResponse.Message),
+                EResponseType.NotFound => NotFound(serviceResponse.Message),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        [HttpPut("update/{id}")]
+        public async Task<IActionResult> Update(string id, [FromBody] JobDTO jobDTO)
+        {
+            var serviceResponse = new ServiceResponse<JobDTO>();
+            serviceResponse = await _jobServices.UpdateJobAsync(int.Parse(id), jobDTO);
+            return serviceResponse.ResponseType switch
+            {
+                EResponseType.Success => Ok(serviceResponse.Data),
+                EResponseType.BadRequest => BadRequest(serviceResponse.Message),
+                EResponseType.CannotCreate => BadRequest(serviceResponse.Message),
+                EResponseType.NotFound => NotFound(serviceResponse.Message),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        [HttpDelete("delete/{id}")]
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            var serviceResponse = new ServiceResponse<JobDTO>();
+            serviceResponse = await _jobServices.DeleteJobAsync(int.Parse(id));
+            return serviceResponse.ResponseType switch
+            {
+                EResponseType.Success => Ok(serviceResponse.Data),
+                EResponseType.BadRequest => BadRequest(serviceResponse.Message),
+                EResponseType.CannotCreate => BadRequest(serviceResponse.Message),
+                EResponseType.NotFound => NotFound(serviceResponse.Message),
+                _ => throw new NotImplementedException()
+            };
         }
     }
 }
